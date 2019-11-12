3d6a35507e1cbc97f43586d903c5ca1011df6a25
Samie Shaya <samieshaya@gmail.com>
2019-11-12 11:19:55 +0100

    Add project files.

---
+++ samiesbank/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace samiesbank.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
